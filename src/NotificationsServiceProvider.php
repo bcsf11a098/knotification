666babb6840e1505cb16ff13603ccc7028a71770
Cloud Horizon <cloudhorizon@Clouds-MacBook-Air.local>
2016-01-27 15:35:17 +0100

    Testing repository in vendor

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -6,6 +6,32 @@ use Illuminate\Support\ServiceProvider;
 
 class NotificationsServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->handleConfigs();
+        // $this->handleMigrations();
+        $this->handleViews();
+        // $this->handleTranslations();
+        $this->handleRoutes();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
     public function register()
     {
         return $this->app->bind('notifications', function ($app){
@@ -13,17 +39,30 @@ class NotificationsServiceProvider extends ServiceProvider
         });
     }
 
-    public function boot()
-    {
-        // loading the route file
-        require __DIR__ . "/Http/routes.php";
-
-        // define the path for the view files
-        $this->loadViewsFrom(__DIR__ . '/../views/', 'notifications');
-
-        // define the files which are going to be published
-        /*$this->publishes([
-            __DIR__ . '/migrations/2016_01_26_000000_create_log_notifications_table.php' => base_path('database/migrations/2016_01_26_000000_create_log_notifications_table.php')
-        ]);*/
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
+    private function handleConfigs() {
+        $configPath = __DIR__ . '/../config/notifications.php';
+        $this->publishes([$configPath => config_path('notifications.php')]);
+        $this->mergeConfigFrom($configPath, 'notifications');
+    }
+    private function handleTranslations() {
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'notifications');
+    }
+    private function handleViews() {
+        $this->loadViewsFrom(__DIR__.'/../views', 'notifications');
+        $this->publishes([__DIR__.'/../views' => base_path('resources/views/panic/notifications')]);
+    }
+    private function handleMigrations() {
+        $this->publishes([__DIR__ . '/../migrations' => base_path('database/migrations')]);
+    }
+    private function handleRoutes() {
+        include __DIR__.'/Http/routes.php';
     }
 }
